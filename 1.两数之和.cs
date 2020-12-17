284a1263fedd6025b56fe5a067bd2691717c7891
Devin <mcxxfs@outlook.com>
2020-12-17 15:47:33 +0800

    upload leetcode project files

---
+++ 1.两数之和.cs
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=csharp
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+using System.Collections.Generic;
+
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        return new int[]{0,1};
+    }
+}
+// @lc code=end
